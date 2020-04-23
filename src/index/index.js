83928635cf720b84882cbe175e07cf4a51709ff2
范跃东 <632296950@qq.com>
2020-04-23 20:00:28 +0800

    提交

--- src/index/index.js
+++ src/index/index.js
@@ -1,18 +1,39 @@
 // const baseNumberReg =/^(\+|\-)?\d+(\.\d+(e\d+)?)?/
-const baseNumberReg = /^(\+|\-)?0|(^([1-9]|\.)[0-9]+?(\.[0-9]+(e(\+|\-)?[0-9]+)?))?/
-const testArr = [ '0','+0', '-0', '1', '2', '3', '4.', '4.3', '4.32123123','.1231',1212e12]
-function testReg(reg, arr = testArr, ) {
-  const isNumber = (num => {
+const baseNumberReg = /^(0$|0?\.|[1-9][0-9]*\.?)[0-9]*([eE][\+\-]?[0-9][0-9]*)?$/;
+const testArr = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4.",
+  "4.3",
+  "4.32123123",
+  ".1231",
+  1212e12,
+];
+function testReg(reg, arr = testArr) {
+  const isNumber = (num) => {
     if (!reg.test(num)) {
-      console.log(num)
+      console.log(num);
     }
-    return reg.test(num)
-  })
+    return reg.test(num);
+  };
   if (arr.every(isNumber)) {
-    console.log('success')
-    return true
+    console.log("success");
+    return true;
   }
-  return false
-
+  return false;
 }
+https://github.com/flonny/Frontend-01-Template/tree/master/week02
 testReg(baseNumberReg)
+function UTF8_Encoding(string) {
+  const textEncoder = new TextEncoder();
+  let encoded = textEncoder.encode(string);
+  let encodeValue = "";
+  encoded.forEach((Codepoint) => {
+    encodeValue += `\\x${Codepoint.toString(16)}`;
+  });
+  return encodeValue
+}
+// console.log(UTF8_Encoding("Foo © bar 𝌆 baz ☃ qux", 16));
+console.log(UTF8_Encoding("Foo © bar 𝌆 baz ☃ qux"));
