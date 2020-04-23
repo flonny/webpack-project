783a8c3236f871d069bdeab06b872c61f4132d19
fanyuedong <632296950@qq.com>
2020-04-23 18:06:46 +0800

    react

--- src/index/index.js
+++ src/index/index.js
@@ -1,18 +1,19 @@
-// const baseNumberReg =/^(\+|\-)?\d+(\.\d+(e\d+)?)?/
-const baseNumberReg = /^(\+|\-)?0|(^([1-9]|\.)[0-9]+?(\.[0-9]+(e(\+|\-)?[0-9]+)?))?/
-const testArr = [ '0','+0', '-0', '1', '2', '3', '4.', '4.3', '4.32123123','.1231',1212e12]
-function testReg(reg, arr = testArr, ) {
-  const isNumber = (num => {
-    if (!reg.test(num)) {
-      console.log(num)
-    }
-    return reg.test(num)
-  })
-  if (arr.every(isNumber)) {
-    console.log('success')
-    return true
+import React, { Component } from 'react'
+import ReactDom from 'react-dom'
+class Clock extends Component {
+constructor(props) {
+  super(props)
+  this.state = {
+    date: new Date()
   }
-  return false
-
 }
-testReg(baseNumberReg)
+
+render() {
+  return (
+    <div>
+      {this.state.date.toLocaleTimeString()}
+    </div>);
+}
+}
+ReactDom.render(<Clock />, document.getElementById('root'))
+export default Clock;
